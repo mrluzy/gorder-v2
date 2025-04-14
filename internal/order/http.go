b24e672d1820b71efd5d4a0824e9f0adefcedcda
mrluzy <1061603606@qq.com>
2025-04-14 16:22:21 +0800

    create&update order

--- internal/order/http.go
+++ internal/order/http.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mrluzy/gorder-v2/common/genproto/orderpb"
 	"github.com/mrluzy/gorder-v2/order/app"
+	"github.com/mrluzy/gorder-v2/order/app/command"
 	"github.com/mrluzy/gorder-v2/order/app/query"
 	"net/http"
 )
@@ -12,14 +14,26 @@ type HttpServer struct {
 }
 
 func (h HttpServer) PostCustomerCustomerIDOrder(c *gin.Context, customerID string) {
-	//TODO implement me
-	panic("implement me")
+	var req orderpb.CreateOrderRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	r, err := h.app.Commands.CreateOrder.Handle(c, command.CreateOrder{
+		CustomerID: req.CustomerID,
+		Items:      req.Items,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"customer_id": req.CustomerID, "order_id": r.OrderID})
 }
 
 func (h HttpServer) GetCustomerCustomerIDOrderOrderID(c *gin.Context, customerID string, orderID string) {
 	o, err := h.app.Queries.GetCustomerOrder.Handle(c, query.GetCustomerOrder{
-		CustomerID: "fake-customer-id",
-		OrderID:    "fake-id",
+		CustomerID: customerID,
+		OrderID:    orderID,
 	})
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{"error": err})
