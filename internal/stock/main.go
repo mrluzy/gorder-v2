2ca96dca3ad530b933864ab44e533d6050842c8d
mrluzy <1061603606@qq.com>
2025-04-15 15:11:26 +0800

    consul

--- internal/stock/main.go
+++ internal/stock/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/mrluzy/gorder-v2/common/config"
+	"github.com/mrluzy/gorder-v2/common/discovery"
 	"github.com/mrluzy/gorder-v2/common/genproto/stockpb"
 	"github.com/mrluzy/gorder-v2/common/server"
 	"github.com/mrluzy/gorder-v2/stock/ports"
@@ -26,6 +27,15 @@ func main() {
 	defer cancel()
 
 	application := service.NewApplication(ctx)
+
+	deregisterFn, err := discovery.RegisterToConsul(ctx, serviceName)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer func() {
+		_ = deregisterFn()
+	}()
+
 	switch serverType {
 	case "grpc":
 		server.RunGRPCServer(serviceName, func(server *grpc.Server) {
