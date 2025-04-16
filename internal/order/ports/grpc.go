9afd9d8fd30fd0ea73223e6bb83e23786c0b6fa7
mrluzy <1061603606@qq.com>
2025-04-16 13:16:05 +0800

    order ports grpc

--- internal/order/ports/grpc.go
+++ internal/order/ports/grpc.go
@@ -4,6 +4,12 @@ import (
 	"context"
 	"github.com/mrluzy/gorder-v2/common/genproto/orderpb"
 	"github.com/mrluzy/gorder-v2/order/app"
+	"github.com/mrluzy/gorder-v2/order/app/command"
+	"github.com/mrluzy/gorder-v2/order/app/query"
+	domain "github.com/mrluzy/gorder-v2/order/domain/order"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -11,21 +17,46 @@ type GRPCServer struct {
 	app app.Application
 }
 
+func NewGRPCServer(app app.Application) *GRPCServer {
+	return &GRPCServer{app: app}
+}
+
 func (G GRPCServer) CreateOrder(ctx context.Context, request *orderpb.CreateOrderRequest) (*emptypb.Empty, error) {
-	//TODO implement me
-	panic("implement me")
+	_, err := G.app.Commands.CreateOrder.Handle(ctx, command.CreateOrder{
+		CustomerID: request.CustomerID,
+		Items:      request.Items,
+	})
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &emptypb.Empty{}, nil
 }
 
 func (G GRPCServer) GetOrder(ctx context.Context, request *orderpb.GetOrderRequest) (*orderpb.Order, error) {
-	//TODO implement me
-	panic("implement me")
+	o, err := G.app.Queries.GetCustomerOrder.Handle(ctx, query.GetCustomerOrder{
+		OrderID:    request.OrderID,
+		CustomerID: request.CustomerID,
+	})
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+	return o.ToProto(), nil
 }
 
-func (G GRPCServer) UpdateOrder(ctx context.Context, order *orderpb.Order) (*emptypb.Empty, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func NewGRPCServer(app app.Application) *GRPCServer {
-	return &GRPCServer{app: app}
+func (G GRPCServer) UpdateOrder(ctx context.Context, request *orderpb.Order) (_ *emptypb.Empty, err error) {
+	logrus.Infof("order_grpc||request_in||request=%+v", request)
+	order, err := domain.NewOrder(
+		request.ID, request.CustomerID, request.Status, request.PaymentLink, request.Items,
+	)
+	if err != nil {
+		err = status.Error(codes.Internal, err.Error())
+		return
+	}
+	_, err = G.app.Commands.UpdateOrder.Handle(ctx, command.UpdateOrder{
+		Order: order,
+		UpdateFn: func(ctx context.Context, order *domain.Order) (*domain.Order, error) {
+			return order, nil
+		},
+	})
+	return
 }
