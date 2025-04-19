0203b45e0e605691e24651ba466c6238294aa554
mrluzy <1061603606@qq.com>
2025-04-19 15:07:51 +0800

    viper动态配置

--- internal/common/config/viper.go
+++ internal/common/config/viper.go
@@ -1,19 +1,54 @@
 package config
 
 import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
 
 	"github.com/spf13/viper"
 )
 
-func NewViperConfig() error {
+func init() {
+	if err := NewViperConfig(); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+var once sync.Once
+
+func NewViperConfig() (err error) {
+	once.Do(func() {
+		err = newViperConfig()
+	})
+	return
+}
+
+func newViperConfig() error {
+	relPath, err := getRelativePathCaller()
+	if err != nil {
+		return err
+	}
 	viper.SetConfigName("global")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath("../common/config")
-	viper.AddConfigPath(".. /config")
+	viper.AddConfigPath(relPath)
 	viper.EnvKeyReplacer(strings.NewReplacer("-", "_"))
 	_ = viper.BindEnv("stripe-key", "STRIPE_KEY")
 	_ = viper.BindEnv("endpoint-stripe-secret", "ENDPOINT_STRIPE_SECRET")
 	viper.AutomaticEnv()
 	return viper.ReadInConfig()
+}
+
+func getRelativePathCaller() (relPath string, err error) {
+	callerPwd, err := os.Getwd()
+	if err != nil {
+		return
+	}
+	_, filename, _, _ := runtime.Caller(0)
+	relPath, err = filepath.Rel(callerPwd, filepath.Dir(filename))
+	fmt.Printf("caller from %s, here is %s, relpath is %s", callerPwd, filename, relPath)
+	return
 }
